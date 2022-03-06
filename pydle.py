e3b4516b03fd5667dfffb27ffae42a701c05f758
Alex Papadopoulos <papadopoulos.alexios@gmail.com>
2022-03-07 00:01:02 +0200

    make program change users command line font at the start of the game

--- pydle.py
+++ pydle.py
@@ -35,7 +35,7 @@ class Game:
             if player_guess[i] == self.mystery_word[i] and player_guess[i] in letters:
                 hint += f'{chr(10004)} ' # Letter in correct spot (checkmark)
                 letters.remove(player_guess[i])
-            elif player_guess[i] in self.mystery_word and player_guess[i] in letters:
+            elif player_guess[i] in letters:
                 hint += f'{chr(10033)} ' # Letter is in word (asterisk)
                 letters.remove(player_guess[i])
             else:
@@ -99,12 +99,6 @@ class Board:
             print('\t\t' + ''.join(self.board[i]))
             print('\t\t' + '-'*31)
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.streak = 0
-
 def word_reader(filename): # Reads a text file and generates set containing its words
     with open(filename) as dictfile:
         dic = set(w.upper() for w in dictfile.read().split('\n'))
@@ -116,6 +110,38 @@ clear = lambda: os.system('cls') # Clears the command line
 game = Game()
 testboard = Board()
 testboard.add_word('WEARY', f'{chr(10004)} {chr(10033)} {chr(10033)} {chr(10033)} {chr(10006)} ')
+
+# <-- Changes Font. Credit to Marfisa on stackoverflow.com-->
+
+import ctypes
+
+LF_FACESIZE = 32
+STD_OUTPUT_HANDLE = -11
+
+class COORD(ctypes.Structure):
+    _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+class CONSOLE_FONT_INFOEX(ctypes.Structure):
+    _fields_ = [("cbSize", ctypes.c_ulong),
+                ("nFont", ctypes.c_ulong),
+                ("dwFontSize", COORD),
+                ("FontFamily", ctypes.c_uint),
+                ("FontWeight", ctypes.c_uint),
+                ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+font = CONSOLE_FONT_INFOEX()
+font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+font.nFont = 12
+font.dwFontSize.X = 11
+font.dwFontSize.Y = 18
+font.FontFamily = 54
+font.FontWeight = 400
+font.FaceName = "SimSun-ExtB"
+
+handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+ctypes.windll.kernel32.SetCurrentConsoleFontEx(
+        handle, ctypes.c_long(False), ctypes.pointer(font))
+
 
 # <--- Introduction Message & Game Rules --->
 print('\n\n\t\t[PYDLE]')
